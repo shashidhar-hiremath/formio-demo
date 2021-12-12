0ece73713c5976bea9e699ddbacdb08a078bdc93
shashidhar-hiremath <shashidhar25081993@gmail.com>
2021-12-12 17:58:03 +0530

    add refill

--- src/app/form-render/form-render.component.ts
+++ src/app/form-render/form-render.component.ts
@@ -11,14 +11,30 @@ export class FormRenderComponent implements OnInit {
   schema: any;
   refreshForm: EventEmitter<any> | undefined;
   @ViewChild('formioObj', { static: true }) formioObj: any;
+  public preFilledData = {};
+  public showCustomFormNav = true;
   private SCHEMA_API = './assets/formiodata/formschema.json';
+  private DATA_API = './assets/formiodata/form-data.json';
   constructor(private readonly httpClient: HttpClient, private readonly formDataService: FormDataService){}
 
   ngOnInit(){
     this.httpClient.get(this.SCHEMA_API).subscribe(x => {
       this.schema = x;
       this.formDataService.createHeader(this.schema);
-    })
+    });
   }
 
+  onSubmit(event: any) {
+    console.log(event.data)
+  }
+
+  addPrefilledData(event: any) {
+    this.httpClient.get(this.DATA_API).subscribe(x => {
+      this.preFilledData = x;
+    });
+  }
+
+  toggleNav(event: any) {
+    this.showCustomFormNav = !this.showCustomFormNav;
+  }
 }
